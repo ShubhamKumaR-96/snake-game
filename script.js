2e84145a53bb440a637ecd693bf940731e31dbdb
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-03-12 22:38:36 +0530

    setup

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    const gameArena=document.getElementById('game-arena')
+    const arenaSize=600;
+    const sellSize=20;
+    let score=0;  // score the game
+    let gameStarted=false // Game status
+    let food={x:300,y:300} // cell coordinate -> pixels
+    let snake=[{x:160,y:200}]
+
+})
