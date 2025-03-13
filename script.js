cddc547b507e229fff13a783e42ce1c091ba8027
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-03-13 22:14:56 +0530

    added style and button

--- script.js
+++ script.js
@@ -6,6 +6,46 @@ document.addEventListener('DOMContentLoaded', function (){
     let score=0;  // score the game
     let gameStarted=false // Game status
     let food={x:300,y:300} // cell coordinate -> pixels
-    let snake=[{x:160,y:200}]
+    let snake=[{x:160,y:200},{x:140,y:200},{x:120,y:200}]
+
+
+    function drawDiv(x,y,className){
+        const divElement=document.createElement('div')
+
+    }
+
+    function drawFoodAndSnake(){
+        gameArena.innerHTML=''
+
+        const foodElement=drawDiv(food.x,food.y, 'food')
+    }
+
+
+    function runGame(){
+        if(!gameStarted){
+            gameStarted=true
+
+        }
+    }
+
+    function initiateGame(){
+        const scoreBoard=document.createElement("div")
+        scoreBoard.id='score-board'
+
+        document.body.insertBefore(scoreBoard,gameArena) // Insert score board before arena
+
+        const startButton=document.createElement('button')
+        startButton.textContent='Start Game'
+        startButton.classList.add('start-button')
+
+        startButton.addEventListener('click',function startGame(){
+            startButton.style.display='none';
+
+            runGame()
+        })
+
+        document.body.appendChild(startButton) // Append start button to the body
+    }
+    initiateGame()
 
 })
